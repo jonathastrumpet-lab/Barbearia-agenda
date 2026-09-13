5536bdf6a48effaab658a40a6a963fc1383705b8
jonathastrumpet-lab <jonathas.trumpet@gmail.com>
2026-09-13 14:39:31 -0300

    test: initialize Supabase before secure login widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,14 @@
+import 'package:barbearia_agenda/appointment_store.dart';
 import 'package:barbearia_agenda/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    await AppointmentStore.initializeCloud();
+  });
+
   testWidgets('exibe login seguro antes de abrir a agenda', (tester) async {
     await tester.pumpWidget(const BarbeariaAgendaApp());
     await tester.pumpAndSettle();
