f5b5cd751464997fbac0e32768d1997a317a7cf3
jonathastrumpet-lab <jonathas.trumpet@gmail.com>
2026-09-13 07:46:41 -0300

    test: update home screen assertions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,13 @@ import 'package:barbearia_agenda/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('exibe a tela inicial da barbearia', (tester) async {
+  testWidgets('exibe a tela inicial profissional da barbearia', (tester) async {
     await tester.pumpWidget(const BarbeariaAgendaApp());
 
     expect(find.text('Barbearia Agenda'), findsOneWidget);
-    expect(find.text('Agenda da Barbearia'), findsOneWidget);
+    expect(find.text('Agendar horário'), findsOneWidget);
+    expect(find.text('Meus agendamentos'), findsOneWidget);
+    expect(find.text('Serviços'), findsOneWidget);
+    expect(find.text('Barbeiros'), findsOneWidget);
   });
 }
